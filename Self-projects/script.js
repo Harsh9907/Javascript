0f99c0f3877a553ed9086dc768908c744b7d7e20
Harsh9907 <harshdeep0029@gmail.com>
2023-09-20 23:06:34 +0530

    Adding in self-projects

--- Self-projects/script.js
+++ Self-projects/script.js
@@ -1,5 +1,25 @@
-let num = Number(document.querySelector(".input").value);
+let num = document.querySelector(".input");
 
 document.querySelector(".btn").addEventListener("click", function () {
-  console.log(num);
+  console.log(typeof Number(num.value));
+  console.log("button clicked!!!");
 });
+
+document.querySelector(".mouse").addEventListener("click", function () {
+  console.log("mouse hovered");
+});
+
+// Get the input element
+// const inputElement = document.querySelector("input");
+
+// // Get the button element
+// const buttonElement = document.querySelector("button");
+
+// // Add an event listener to the button element
+// buttonElement.addEventListener("click", function () {
+//   // Get the value of the input element
+//   const inputValue = Number(inputElement.value);
+//   console.log(typeof inputValue);
+//   // Log the input value to the console
+//   console.log(inputValue);
+// });
